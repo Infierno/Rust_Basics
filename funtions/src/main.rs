eb23c02e95312185b22f26cf93fc6b69e5310580
Infierno <u.n.owen.infierno@gmail.com>
2025-10-10 23:50:58 -0600

    Manejo de Funciones Basico.
    Manejo basico de funciones este es solo para referencia si alguien lo necesita.

---
+++ funtions/src/main.rs
@@ -0,0 +1,13 @@
+fn reboot_optics(x:i32, y:i32) { //recibe los argumentos como entero 
+                                 //a 32bits y los imprime.
+
+    println!("{},{}",x,y);
+}
+
+fn main () {
+
+    reboot_optics(20,10); //manda los valores de "x" y "y" a
+                                //la funcion reboot_optics como
+                                //argumentos y corre la funcion.
+
+}
